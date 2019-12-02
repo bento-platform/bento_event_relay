aa84c16b94524f53f34b70a26716bde6fe0a6a49
David Lougheed <david.lougheed@gmail.com>
2019-12-02 12:14:40 -0500

    Add basic event relay code

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const redis = require("redis");
+const socketIO = require("socket.io");
+
+const app = http.createServer((_, res) => {
+    res.writeHead(404);
+    res.end();
+});
+
+const client = redis.createClient(process.env.REDIS_SOCKET || {});
+const io = socketIO(app);
+
+io.on("connection", socket => {
+    // TODO: Multiple clients??
+    client.on("pmessage", (_, message) => {
+        // TODO: Filter message type in relay
+        // TODO: Catch parse exceptions
+        try {
+            const messageData = JSON.parse(message);
+            socket.emit("events", messageData);
+        } catch (e) {
+            console.error(`Encountered error while relaying message: ${e}`);
+        }
+    });
+});
+
+app.listen(process.env.SERVICE_SOCKET || 8080);
