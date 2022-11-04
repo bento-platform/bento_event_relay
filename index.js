c2d3c66b4791ec0f10e68c6e6dcbd2934aded724
David Lougheed <david.lougheed@gmail.com>
2022-11-04 12:18:39 -0400

    feat: update dependencies, fix redis connection, fix service type response

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ const pj = require("./package");
 
 const parseIntIfInt = v => v && v.toString().match(/^\d+$/) ? parseInt(v, 10) : v;
 
-const SERVICE_TYPE = `ca.c3g.bento:event-relay:${pj.version}`;
-const SERVICE_ID = process.env.SERVICE_ID || SERVICE_TYPE;
+const SERVICE_TYPE = {
+    "group": "ca.c3g.bento",
+    "artifact": "event-relay",
+    "version": pj.version,
+};
+const SERVICE_ID = process.env.SERVICE_ID || Object.values(SERVICE_TYPE).slice(0, 2).join(":");
 
 const SERVICE_INFO = {
     "id": SERVICE_ID,
@@ -30,7 +34,7 @@ const SERVICE_INFO = {
 };
 
 const JSON_MESSAGES = (process.env.JSON_MESSAGES || "true").trim().toLocaleLowerCase() === "true";
-const REDIS_CONNECTION = process.env.REDIS_CONNECTION;
+const REDIS_CONNECTION = process.env.REDIS_CONNECTION || "redis://localhost:6379";
 const REDIS_SUBSCRIBE_PATTERN = process.env.REDIS_SUBSCRIBE_PATTERN || "chord.*";
 const SERVICE_URL_BASE_PATH = process.env.SERVICE_URL_BASE_PATH || "";
 const SOCKET_IO_PATH = process.env.SOCKET_IO_PATH || "/socket.io";
@@ -42,7 +46,7 @@ const SERVICE_LISTEN_ON = parseIntIfInt(process.env.SERVICE_LISTEN_ON || process
 
 const app = http.createServer((req, res) => {
     // Only respond to /service-info requests (to be CHORD-compatible)
-    if (req.url === `${SERVICE_URL_BASE_PATH}/service-info`) {
+    if (req.url.startsWith(`${SERVICE_URL_BASE_PATH}/service-info`)) {
         res.writeHead(200, {"Content-Type": "application/json"});
         res.end(JSON.stringify(SERVICE_INFO));
         return;
@@ -58,32 +62,49 @@ const io = new socketIO.Server(app, {
 });
 
 // Create a pub-sub client to listen for events
-const client = redis.createClient(REDIS_CONNECTION || {});
-
-// Forward any message received to all currently-open socket.io connections
-client.on("pmessage", (pattern, channel, message) => {
-    // TODO: Filter message type in relay
-    for (const socket of io.of("/").sockets.values()) {
-        try {
-            // Include channel in message data, since otherwise the information is lost on the receiving end.
-            // If we're in "generic mode" (i.e. JSON_MESSAGES is false) then the message is passed as the single key in
-            // an event object; otherwise, pass the deserialized data.
-            socket.emit("events", {message: JSON_MESSAGES ? JSON.parse(message) : message, channel});
-        } catch (e) {
-            console.error(`Encountered error while relaying message: ${e} (pattern: ${pattern}, channel: ${
-                channel}, message: ${message})`);
-        }
-    }
+const client = redis.createClient({
+    url: REDIS_CONNECTION,
 });
 
-// Subscribe to all incoming messages
-client.psubscribe(REDIS_SUBSCRIBE_PATTERN);
 
-// Listen on a socket file or port
-app.listen(SERVICE_LISTEN_ON);
-console.log(`bento_event_relay listening on ${SERVICE_LISTEN_ON}`);
+(async () => {
+    await client.connect();
+
+    // Subscribe to all incoming messages
+    // Forward any message received to all currently-open socket.io connections
+    await client.pSubscribe(REDIS_SUBSCRIBE_PATTERN, (message, channel) => {
+        // TODO: Filter message type in relay
+        for (const socket of io.of("/").sockets.values()) {
+            try {
+                // Include channel in message data, since otherwise the information is lost on the receiving end.
+                // If we're in "generic mode" (i.e. JSON_MESSAGES is false) then the message is passed as the single key in
+                // an event object; otherwise, pass the deserialized data.
+                socket.emit("events", {message: JSON_MESSAGES ? JSON.parse(message) : message, channel});
+            } catch (e) {
+                console.error(`Encountered error while relaying message: ${e} (pattern: ${pattern}, channel: ${
+                    channel}, message: ${message})`);
+            }
+        }
+    });
+
+    // Listen on a socket file or port
+    app.listen(SERVICE_LISTEN_ON);
+    console.log(`bento_event_relay listening on ${SERVICE_LISTEN_ON}`);
+    console.log("config:");
+    console.log(`\tJSON_MESSAGES=${JSON_MESSAGES}`);
+    console.log(`\tREDIS_CONNECTION=${REDIS_CONNECTION}`);
+    console.log(`\tREDIS_SUBSCRIBE_PATTERN=${REDIS_SUBSCRIBE_PATTERN}`);
+    console.log(`\tSERVICE_URL_BASE_PATH=${SERVICE_URL_BASE_PATH}`);
+    console.log(`\tSOCKET_IO_PATH=${SOCKET_IO_PATH}`);
+    console.log(`\tSERVICE_LISTEN_ON=${SERVICE_LISTEN_ON}`);
+})();
 
 // Properly shut down (thus cleaning up any open socket files) when a signal is received
-const shutdown = () => app.close(() => process.exit());
+const shutdown = () => {
+    (async () => {
+        await client.disconnect();
+        app.close(() => process.exit());
+    })();
+}
 process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
