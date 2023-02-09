46a8f5552391d1c81f6fbabbc9da9511e6a9b946
David Lougheed <david.lougheed@gmail.com>
2023-02-09 13:56:47 -0500

    use gitconfig setting script

--- entrypoint.dev.bash
+++ entrypoint.dev.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Set .gitconfig for development
+/set_gitconfig.bash
+
 if [ -z "${INTERNAL_PORT}" ]; then
   # Set default internal port to 8080
   export INTERNAL_PORT=8080
 fi
 
+# Install dependencies
 npm ci
+
+# Nodemon: run server and restart with any changes
 npx nodemon -L
